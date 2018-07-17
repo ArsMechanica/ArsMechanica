16408f32e0f02ad0188fad24e5f4764eea84c4b6
ArsMechanica <githab@ars-mechanica.ru>
2018-07-17 21:23:04 +0300

    test

---
+++ src/ArsMechanica/Registry/RequestRegistry.php
@@ -0,0 +1,44 @@
+<?php
+namespace ArsMechanica\Registry;
+
+use ArsMechanica\Controller\Request;
+
+
+class RequestRegistry extends AbstractRegistry {
+
+    use \ArsMechanica\Interfaces\Singleton;
+
+    private $RequestParametrList = [];
+
+    private function __construct() {}
+	
+    public function get($key) {
+        if($this->isSet($key)) {
+            return $this->RequestParametrList[$key];
+            }
+        return NULL;
+        }
+	
+    public function set($key, $value):void {
+        $this->RequestParametrList[$key] = $value;
+        }
+
+	public function isSet($key):bool {
+        if(array_key_exists($key, $this->RequestParametrList)) {
+            return TRUE;
+        }
+        return NULL;
+    }
+
+    public function unset($key):void {
+        unset($this->RequestParametrList[$key]);
+    }
+
+    static function getRequest():Request {
+	    return self::getInstance()->get('request');
+	    }
+
+    static function setRequest(Request $Request) {
+        return self::getInstance()->set('request', $Request);
+        }
+    }
---
+++ src/ArsMechanica/Registry/SessionRegistry.php
@@ -0,0 +1,45 @@
+<?php
+namespace ArsMechanica\Registry;
+
+class SessionRegistry extends AbstractRegistry  {
+    use \ArsMechanica\Interfaces\Singleton;
+
+    private function __construct() {
+        if(session_status() !== PHP_SESSION_ACTIVE) {
+            session_start();
+            }
+        }
+
+    public function get($key) {
+        if(self::getInstance()->isSet($key)) {
+            return $_SESSION[__CLASS__][$key];
+            }
+        return NULL;
+        }
+	
+    public function set($key, $value):void {
+        $_SESSION[__CLASS__][$key] = $value;
+        }
+
+    public function isSet($key):bool {
+        return isset($_SESSION[__CLASS__][$key]);
+        }
+
+    public function unset($key):void {
+        if(self::getInstance()->isSet($key)) {
+            unset($_SESSION[__CLASS__][$key]);
+        }
+    }
+
+    static function setLanguage($lang_code_str) {
+        return self::getInstance()->set('lang_code', $lang_code_str);
+        }
+
+    static function getComplex() {
+        return self::getInstance()->get('complex');
+        }
+
+    static function setRequest(Complex $Complex) {
+        return self::getInstance()->set('complex', $Complex);
+        }
+    }
